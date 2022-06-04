95fa915ca574dce8344dbbbacb327b668b40be37
souvikroy222 <69359546+souvikroy222@users.noreply.github.com>
2022-06-04 17:01:45 +0530

    product functionality done

--- models/productModel.js
+++ models/productModel.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const productSchema = mongoose.Schema({
+/*const productSchema = mongoose.Schema({
   brand: {
     type: String,
     required: true,
@@ -64,5 +64,70 @@ const productSchema = mongoose.Schema({
 },{
     timestamps:true
 });
+*/
 
-const Product=mongoose.model("Products",productSchema)
+const productSchema = mongoose.Schema(
+  {
+    brand: {
+      type: String,
+      required: true,
+    },
+    inventory: {
+      type: Number,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    newPrice: {
+      type: Number,
+      required: true,
+    },
+    oldPrice: {
+      type: Number,
+      required: true,
+    },
+    offer: {
+      type: String,
+      required: true,
+    },
+    images: {
+      type: [],
+    },
+    country_of_origin: {
+      type: String,
+      required: true,
+    },
+    customer_care_details: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    disclaimer: {
+      type: String,
+      required: true,
+    },
+    key_features: {
+      type: String,
+      required: true,
+    },
+    manufacturer_details: {
+      type: String,
+      required: true,
+    },
+    shelf_life: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Product = mongoose.model("Products", productSchema);
+module.exports=Product
